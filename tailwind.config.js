4e2944d728e8fef522e9017d7d2c7f471e63b13d
niaanzariani23 <niaanzariani@gmail.com>
2023-12-20 14:21:49 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    fontFamily: {
+      'poppins' : ['poppins', 'sans-serif'],
+      'teko' : ['teko', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
